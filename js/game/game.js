9cc2eb7d7d4a78415ff0e8e84d49369adc4969e9
Ulrich <ulrich.saukel@gmail.com>
2015-04-15 14:57:59 -0300

    new assets - added some of necessary "class"
    created bitmap font set, purchased audio loop sound track and integrated
    - layout together and implementing design of games.

--- js/game/game.js
+++ js/game/game.js
@@ -1,24 +1,32 @@
 //javascript
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
-var version = "[Summer is coming] - Demo Game - Ulrich.S - 15/04/2015";
+var version = "[Summer is coming] - Demo Game - Ulrich.Saukel - 15/04/2015";
 var gameContainer;
 var style;
- var text;
+var text;
+
+var soundtrack;
+var initialBet = 100;
 
 function preload() {
 	game.load.image('background','/game/assets/images/background.png');
-	game.load.image('board','/game/assets/imagesboard.png');
-	game.load.image('logo','/game/assets/imageslogo.png');
+	game.load.image('board','/game/assets/images/board.png');
+	game.load.image('logo','/game/assets/images/logo.png');
+	game.load.audio('summer',['/game/assets/sounds/Feel the Summer.mp3']);
 }
 
 function create() {
 
+	soundtrack = game.add.audio('summer');
+	soundtrack.volume = 0.2;
+	soundtrack.loop = true;
+	soundtrack.play();
 
 	game.add.sprite(0,0,'background');
 	game.add.sprite(0,0,'board');
-	game.add.sprite(25,25,'logo');
+	game.add.sprite(575,124,'logo');
 
-	style = { font: "15px Arial", fill: "#333333", align: "right" };
+	style = { font: "17px Arial", fill: "#ffffff", align: "right" };
     text = game.add.text(game.world.centerY, 570, version, style);
 
     console.log("[created]");
